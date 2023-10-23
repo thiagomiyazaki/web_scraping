ee67917f02c9615fe77af53a380f708d4228bc38
thiagomiyazaki <miyathiago@gmail.com>
2023-10-23 17:39:41 +0000

    add req

--- teste_df_pandas.py
+++ teste_df_pandas.py
@@ -3,13 +3,10 @@ import pandas as pd
 states = ["São Paulo", "Minas Gerais", "Rio de Janeiro", "Bahia"]
 population = [44420459, 20538718, 16054524, 14136417]
 
-dict_states = {
-    'Estados': states,
-    "População": population
-}
+dict_states = {'Estados': states, "População": population}
 
 df_states = pd.DataFrame.from_dict(dict_states)
 
 # print(df_states)
 
-df_states.to_csv('estados.csvv', index=False)
+df_states.to_csv('estados.csv', index=False)
